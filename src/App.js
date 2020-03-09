9e8f3aa1320595ae65baa556b56d4d36da40b4dd
Julian Diaz <judadi1994@gmail.com>
2020-03-09 14:46:33 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+ ApolloClient,
+ ApolloProvider,
+ createBatchingNetworkInterface,
+} from 'react-apollo'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import HomeView from './views/HomeView'
+import CreateView from './views/CreateView'
+import DetailView from './views/DetailView'
+const networkInterface = createBatchingNetworkInterface({
+  uri: 'http://0.0.0.0:8000/gql/',
+  batchInterval: 10,
+  opts: {
+  credentials: 'same-origin',
+  },
+ })
+ const client = new ApolloClient({
+  networkInterface: networkInterface,
+ })
 
-function App() {
+ class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  <ApolloProvider client={client}>
+  <Router>
+  <div>
+  <Route exact path="/" component={HomeView} />
+  <Switch>
+  <Route exact path="/messages/create/" component={CreateView} />
+  <Route exact path="/messages/:id/" component={DetailView} />
+  </Switch>
+  </div>
+  </Router>
+  </ApolloProvider>
+  )
+  }
+ }
+ export default App
